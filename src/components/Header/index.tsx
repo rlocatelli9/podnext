000a051a28e62b7aace2c0d9e37ad7ac41597109
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-04-20 22:22:26 -0300

    feat: first commit with creation of Header and Player Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+const Header: React.FC = () => {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  });
+  
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="logo"/>
+
+      <p>O melhor para você ouvir, sempre!</p>
+
+      <span>{currentDate}</span>
+    </header>
+  )
+}
+
+export default Header;
